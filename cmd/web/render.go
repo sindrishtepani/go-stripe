f56f23a7ca1fad540f62ee98ce2222473d2429b2
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-07-12 20:56:02 -0600

    changed tmpl to gohtml

--- cmd/web/render.go
+++ cmd/web/render.go
@@ -28,13 +28,14 @@ var functions = template.FuncMap{}
 var templateFS embed.FS
 
 func (app *application) addDefaultData(td *templateData, r *http.Request) *templateData {
+	td.API = app.config.api
 	return td
 }
 
 func (app *application) renderTemplate(w http.ResponseWriter, r *http.Request, page string, td *templateData, partials ...string) error {
 	var t *template.Template
 	var err error
-	templateToRender := fmt.Sprintf("templates/%s.page.tmpl", page)
+	templateToRender := fmt.Sprintf("templates/%s.page.gohtml", page)
 
 	_, templateInMap := app.templateCache[templateToRender]
 
@@ -70,21 +71,21 @@ func (app *application) parseTemplate(partials []string, page, templateToRender 
 
 	if len(partials) > 0 {
 		for i, x := range partials {
-			partials[i] = fmt.Sprintf("templates/%s.partial.tmpl", x)
+			partials[i] = fmt.Sprintf("templates/%s.partial.gohtml", x)
 		}
 	}
 
 	if len(partials) > 0 {
-		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).
 			Funcs(functions).
 			ParseFS(templateFS,
-				"templates/base.layout.tmpl",
+				"templates/base.layout.gohtml",
 				strings.Join(partials, ","), templateToRender)
 	} else {
-		t, err = template.New(fmt.Sprintf("%s.page.tmpl", page)).
+		t, err = template.New(fmt.Sprintf("%s.page.gohtml", page)).
 			Funcs(functions).
 			ParseFS(templateFS,
-				"templates/base.layout.tmpl", templateToRender)
+				"templates/base.layout.gohtml", templateToRender)
 	}
 
 	if err != nil {
