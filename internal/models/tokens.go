619257a98ed40c4cdf73c3d1ac4c22fb55988474
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-07-18 19:41:36 -0600

    changed token to include expiry date and get user by token

--- internal/models/tokens.go
+++ internal/models/tokens.go
@@ -52,8 +52,8 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 	}
 
 	stmt = `
-	insert into tokens (user_id, name, email, token_hash, created_at, updated_at)
-	values (?, ?, ?, ?, ?, ?)`
+	insert into tokens (user_id, name, email, token_hash, created_at, updated_at, expiry_date)
+	values (?, ?, ?, ?, ?, ?, ?)`
 
 	_, err = m.DB.ExecContext(ctx, stmt,
 		u.Id,
@@ -62,10 +62,38 @@ func (m *DBModel) InsertToken(t *Token, u User) error {
 		t.Hash,
 		time.Now(),
 		time.Now(),
+		t.Expiry,
 	)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (m *DBModel) GetUserByToken(token string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	tokenHash := sha256.Sum256([]byte(token))
+	var user User
+
+	query := `
+	select users.id, users.first_name, users.last_name, users.email 
+	from users
+	inner join tokens
+	on users.id = tokens.user_id
+	where tokens.token_hash = ? and tokens.expiry_date > ?`
+
+	err := m.DB.QueryRowContext(ctx, query, tokenHash[:], time.Now()).Scan(
+		&user.Id,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
