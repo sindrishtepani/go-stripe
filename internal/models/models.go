02fed21ae65fe671f4800ae5a940a5d2cf842cbb
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-08-04 18:28:53 -0700

    wrote db query for full orders object query

--- internal/models/models.go
+++ internal/models/models.go
@@ -42,15 +42,18 @@ type Widget struct {
 }
 
 type Order struct {
-	Id            int       `json:"id"`
-	WidgetId      int       `json:"widget_id"`
-	CustomerId    int       `json:"customer_id"`
-	TransactionId int       `json:"transaction_id"`
-	StatusId      int       `json:"status_id"`
-	Quantity      int       `json:"quantity"`
-	Amount        int       `json:"amount"`
-	CreatedAt     time.Time `json:"-"`
-	UpdatedAt     time.Time `json:"-"`
+	Id            int         `json:"id"`
+	WidgetId      int         `json:"widget_id"`
+	CustomerId    int         `json:"customer_id"`
+	TransactionId int         `json:"transaction_id"`
+	StatusId      int         `json:"status_id"`
+	Quantity      int         `json:"quantity"`
+	Amount        int         `json:"amount"`
+	CreatedAt     time.Time   `json:"-"`
+	UpdatedAt     time.Time   `json:"-"`
+	Widget        Widget      `json:"widget"`
+	Transaction   Transaction `json:"transaction"`
+	Customer      Customer    `json:"customer"`
 }
 
 type Status struct {
@@ -286,4 +289,87 @@ func (m *DBModel) Authenticate(email, password string) (int, error) {
 	}
 
 	return id, nil
+}
+
+func (m *DBModel) UpdatePasswordForUser(u User, hash string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `update users set password = ? where id = ?`
+
+	_, err := m.DB.ExecContext(ctx, stmt, hash, u.Id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m DBModel) GetAllOrders() ([]*Order, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var orders []*Order
+
+	query := `
+	select
+		o.id, o.widget_id, o.transaction_id, o.customer_id, 
+		o.status_id, o.quantity, o.amount, o.created_at,
+		o.updated_at, w.id, w.name, t.id, t.amount, t.currency,
+		t.last_four, t.expiry_month, t.expiry_year, t.payment_intent,
+		t.bank_return_code, c.id, c.first_name, c.last_name, c.email
+		
+	from
+		orders o
+		left join widgets w on (o.widget_id = w.id)
+		left join transactions t on (o.transaction_id = t.id)
+		left join customers c on (o.customer_id = c.id)
+	where
+		w.is_recurring = 0
+	order by
+		o.created_at desc
+	`
+
+	rows, err := m.DB.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var o Order
+
+		err = rows.Scan(
+			&o.Id,
+			&o.WidgetId,
+			&o.TransactionId,
+			&o.CustomerId,
+			&o.StatusId,
+			&o.Quantity,
+			&o.Amount,
+			&o.CreatedAt,
+			&o.UpdatedAt,
+			&o.Widget.Id,
+			&o.Widget.Name,
+			&o.Transaction.Id,
+			&o.Transaction.Amount,
+			&o.Transaction.Currency,
+			&o.Transaction.LastFour,
+			&o.Transaction.ExpiryMonth,
+			&o.Transaction.ExpiryYear,
+			&o.Transaction.PaymentIntent,
+			&o.Transaction.BankReturnCode,
+			&o.Customer.Id,
+			&o.Customer.FirstName,
+			&o.Customer.LastName,
+			&o.Customer.Email,
+		)
+		if err != nil {
+			return nil, err
+		}
+		orders = append(orders, &o)
+	}
+
+	return orders, nil
+
 }
