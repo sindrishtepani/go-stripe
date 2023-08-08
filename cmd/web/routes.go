0a5e4e713e6a7fb6954743c1819e0c8986e59eb5
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-08-08 14:25:11 -0700

    front end changes to implement all sales/subs and single sub/sales

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,9 @@ func (app *application) routes() http.Handler {
 		mux.Get("/virtual-terminal", app.VirtualTerminal)
 		mux.Get("/all-sales", app.AllSales)
 		mux.Get("/all-subscriptions", app.AllSubscriptions)
+
+		mux.Get("/sales/{id}", app.ShowSale)
+		mux.Get("/subscriptions/{id}", app.ShowSubscription)
 	})
 
 	mux.Get("/widget/{id}", app.ChargeOnce)
@@ -33,7 +36,6 @@ func (app *application) routes() http.Handler {
 	mux.Get("/logout", app.Logout)
 	mux.Get("/forgot-password", app.ForgotPassword)
 	mux.Get("/reset-password", app.ShowResetPassword)
-
 
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
