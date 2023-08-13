50ce9919a7d1b62add6b4e9428778ffbaeecba24
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-08-13 00:26:14 -0600

    front end modifications for users

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	mux.Use(SessionLoad)
 
 	mux.Get("/", app.Home)
+	mux.Get("/ws", app.WsEndPoint)
 
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.Auth)
@@ -21,6 +22,9 @@ func (app *application) routes() http.Handler {
 
 		mux.Get("/sales/{id}", app.ShowSale)
 		mux.Get("/subscriptions/{id}", app.ShowSubscription)
+
+		mux.Get("/all-users", app.AllUsers)
+		mux.Get("/all-users/{id}", app.OneUser)
 	})
 
 	mux.Get("/widget/{id}", app.ChargeOnce)
