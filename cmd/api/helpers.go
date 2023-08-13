5276ad7f2b953783da22b22d0891418e3b3d2837
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-08-13 17:21:27 -0600

    helper function to send json for any failed validation rules

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -66,3 +66,17 @@ func (app *application) badRequest(w http.ResponseWriter, r *http.Request, err e
 
 	return nil
 }
+
+func (app *application) failedValidation(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	var payload struct {
+		Error   bool              `json:"error"`
+		Message string            `json:"message"`
+		Errors  map[string]string `json:"errors"`
+	}
+
+	payload.Error = true
+	payload.Message = "failed validation"
+	payload.Errors = errors
+
+	app.writeJSON(w, http.StatusUnprocessableEntity, payload)
+}
